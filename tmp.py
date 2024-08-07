906ad32322256f6e7d1c41c0722fca77cbfbc2a1
Kimjojojojojo <kbj010205@naver.com>
2024-08-07 17:41:17 +0900

    24/08/07
    DCP Error exists

--- tmp.py
+++ tmp.py
@@ -26,26 +26,6 @@ def make_psd(matrix):
     psd_matrix = np.dot(eigvecs, np.dot(np.diag(eigvals), eigvecs.conj().T))
     return psd_matrix
 
-
-def generate_gaussian_channel(K, M, Q, sigma_e):
-    H = np.zeros((K, M, Q), dtype=complex)
-    for k in range(K):
-        real_part = np.random.normal(0, sigma_e, (M, Q))
-        imag_part = np.random.normal(0, sigma_e, (M, Q))
-        H[k] = (real_part + 1j * imag_part) / np.sqrt(2 * sigma_e)
-
-    threshold = 10^(-5)
-    for k in range(K):
-        H_kq = H[k]
-        # SVD
-        U, S, Vh = np.linalg.svd(H_kq, full_matrices=False)
-        # Thresholding
-        S[S < threshold] = 0
-        # Reconstruct matrix
-        H[k] = U @ np.diag(S) @ Vh
-
-    return H
-
 M = 4
 Q = 2
 K = 2
@@ -55,9 +35,9 @@ P_t = 10
 sigma_e = (P_t) ** (-1*alpha)
 sigma_n = 1
 
-H_true = generate_gaussian_channel(K, M, Q, 1)
-H_error = generate_gaussian_channel(K, M, Q, sigma_e)
-H_hat = H_true - H_error
+H_sample = (np.random.normal(0, 1, (K, M, Q)) + 1j*np.random.normal(0, 1, (K, M, Q))) / np.sqrt(2)
+H_error = (np.random.normal(0, sigma_e, (K, M, Q)) + 1j*np.random.normal(0, sigma_e, (K, M, Q))) / np.sqrt(2*sigma_e)
+H_hat = H_sample - H_error
 H_concat = np.hstack(H_hat)
 
 
@@ -88,10 +68,6 @@ a_pk_tmp=[]
 Phi_ck_tmp=[]
 Phi_pk_tmp=[]
 
-# Interference term
-R_ck = np.zeros((K, Q, Q), dtype=complex)
-R_pk = np.zeros((K, Q, Q), dtype=complex)
-
 # CVX variables
 mu = np.ones(K)
 X_hat = cp.Variable(K)
@@ -104,7 +80,7 @@ print(H_hat)
 # AO Algorithm
 for n in range(iter_num):
     print(f"{n}th iteration")
-    H_tilde = generate_gaussian_channel(K, M, Q, sigma_e)
+    H_tilde = (np.random.normal(0,sigma_e, (K, M, Q)) + 1j*np.random.normal(0,sigma_e, (K, M, Q))) / np.sqrt(2*sigma_e)
     H_k = H_hat + H_tilde
     H_k_H = np.zeros((K, Q, M), dtype= complex)
     #print(H_k)
@@ -113,11 +89,13 @@ for n in range(iter_num):
         eigenvalues = np.linalg.eigvals(H_k[k] @ H_k_H[k])
         for eig in eigenvalues:
             if eig < 0:
-                print(f"H_ck[{k}] Eigenvalues:", eig)
+                print("U_ck[k] Eigenvalues:", eig)
 
 
 
     # Generate interference covariance matrices
+    R_ck = np.zeros((K,Q,Q), dtype= complex)
+    R_pk = np.zeros((K,Q,Q), dtype= complex)
     for k in range(K):
         R_ck[k] = np.eye(Q) + np.sum([H_k_H[k] @ P_k[i] @ P_k_H[i] @ H_k[k] for i in range(K)])
         R_pk[k] = R_ck[k] - H_k_H[k] @ P_k[k] @ P_k_H[k] @ H_k[k]
@@ -148,6 +126,8 @@ for n in range(iter_num):
         U_pk[k] = np.eye(Q) + P_k_H[k] @ H_k[k] / (R_pk[k]) @ H_k_H[k] @ P_k[k]
         U_ck_H[k] = np.conjugate(U_ck[k]).T
         U_pk_H[k] = np.conjugate(U_pk[k]).T
+        eigenvalues = np.linalg.eigvals(U_ck[k])
+
 
 
     # Vectorization
@@ -171,9 +151,9 @@ for n in range(iter_num):
         a_ck[k] = vectorization(H_k[k] @ G_ck_H[k] @ U_ck_H[k])
         a_pk[k] = vectorization(H_k[k] @ G_pk_H[k] @ U_pk_H[k])
         Phi_ck[k] = np.real(
-            sigma_n * np.trace(U_ck[k] @ G_ck[k] @ G_ck_H[k]) + np.trace(U_ck[k]) - np.log2(np.linalg.det(U_ck[k])))
+            1 * np.trace(U_ck[k] @ G_ck[k] @ G_ck_H[k]) + np.trace(U_ck[k]) - np.log2(np.linalg.det(U_ck[k])))
         Phi_pk[k] = np.real(
-            sigma_n * np.trace(U_pk[k] @ G_pk[k] @ G_pk_H[k]) + np.trace(U_pk[k]) - np.log2(np.linalg.det(U_pk[k])))
+            1 * np.trace(U_pk[k] @ G_pk[k] @ G_pk_H[k]) + np.trace(U_pk[k]) - np.log2(np.linalg.det(U_pk[k])))
 
     A_prime_ck_tmp.append(A_prime_ck)
     A_ck_tmp.append(A_ck)
@@ -191,17 +171,18 @@ for n in range(iter_num):
     Phi_hat_ck = np.mean(Phi_ck_tmp, 0)
     Phi_hat_pk = np.mean(Phi_pk_tmp, 0)
 
-### Obtain precoder P through CVX tool ###
+    ### Obtain precoder P through CVX tool ###
     objective_expr = 0
     for k in range(1, K + 1):
         A_hat_pk_symmetric = 0.5 * (A_hat_pk[k - 1] + A_hat_pk[k - 1].conj().T)
-        #print(A_hat_pk_symmetric)
+        A_hat_pk_symmetric_psd = make_psd(A_hat_pk_symmetric)
+
         term1 = X_hat[k - 1]
-        term2 = cp.real(cp.quad_form(P_vector[k], A_hat_pk_symmetric))
+        term2 = cp.real(cp.quad_form(P_vector[k], A_hat_pk_symmetric_psd))
         term3 = 0
         for i in range(1, K + 1):
             if i != k:
-                term3 += cp.quad_form(P_vector[i], A_hat_pk_symmetric)
+                term3 += cp.quad_form(P_vector[i], A_hat_pk_symmetric_psd)
         terms3 = cp.real(term3)
         term4 = -2 * cp.real(a_hat_pk[k - 1].conj().T @ P_vector[k])
         term5 = Phi_hat_pk[k - 1]
@@ -214,13 +195,15 @@ for n in range(iter_num):
 
     for k in range(1, K + 1):
         A_hat_prime_ck_symmetric = 0.5 * (A_hat_prime_ck[k - 1] + A_hat_prime_ck[k - 1].conj().T)
+        A_hat_prime_ck_symmetric_psd = make_psd(A_hat_prime_ck_symmetric)
         A_hat_ck_symmetric = 0.5 * (A_hat_ck[k - 1] + A_hat_ck[k - 1].conj().T)
+        A_hat_ck_symmetric_psd = make_psd(A_hat_ck_symmetric)
 
         lhs = cp.sum(X_hat, axis=0) + Q
-        rhs1 = cp.quad_form(P_vector[0], A_hat_prime_ck_symmetric)
+        rhs1 = cp.quad_form(P_vector[0], A_hat_prime_ck_symmetric_psd)
         rhs2 = 0
         for i in range(1, K + 1):
-            rhs2 += cp.quad_form(P_vector[i], A_hat_ck_symmetric)
+            rhs2 += cp.quad_form(P_vector[i], A_hat_ck_symmetric_psd)
         rhs3 = - 2 * cp.real(a_hat_ck[k - 1].conj().T @ P_vector[0]) + Phi_ck[k - 1]
         rhs = rhs1 + rhs2 + rhs3
         constraints.append(lhs >= rhs)
@@ -231,29 +214,21 @@ for n in range(iter_num):
 
     prob = cp.Problem(objective, constraints)
     result = prob.solve()
+    print("P:", result)
+    if result is not None:
+        P_values = [P_k.value for P_k in P]
+        X_hat_values = X_hat.value
 
-    P_values = [P_k.value for P_k in P]
-    X_hat_values = X_hat.value
-
-    for k in range(K + 1):
-        if k == 0:
-            P_c = P_values[k]
-            P_c_H = np.conjugate(P_c).T
-        else:
-            P_k[k - 1] = P_values[k]
-            P_k_H[k - 1] = np.conjugate(P_k[k - 1]).T
+        for k in range(K + 1):
+            if k == 0:
+                P_c = P_values[k]
+                P_c_H = np.conjugate(P_c).T
+            else:
+                P_k[k - 1] = P_values[k]
+                P_k_H[k - 1] = np.conjugate(P_k[k - 1]).T
+    else:
+        print("최적화 문제에 대한 해를 찾을 수 없습니다.")
 
     # print("최적 값:", result)
     # for k in range(K + 1):
     #     print(f"최적 P[{k}]:\n", P[k].value)
-
-# Calculate data rate
-r_ck = np.zeros(K)
-r_pk = np.zeros(K)
-
-for k in range(K):
-    r_ck[k] = np.log2(np.linalg.det(np.eye(Q) + P_c_H @ H_true[k] / R_ck[k] @ H_true[k].conj().T @ P_c))
-    r_pk[k] = np.log2(np.linalg.det(np.eye(Q) + P_k_H[k] @ H_true[k] / R_pk[k] @ H_true[k].conj().T @ P_k[k]))
-
-print("Common data rate:", np.min(r_ck))
-print("Sum data rate:", np.sum(r_pk))
